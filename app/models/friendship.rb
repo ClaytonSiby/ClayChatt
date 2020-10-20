ac847e554114abe2bd0a5639cb9a26afdcc1caff
Clayton Siby <claytonsiby@gmail.com>
2020-10-20 18:51:24 +0200

    Add inverse associations between user & friendship models

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,7 @@
 class Friendship < ApplicationRecord
-  belongs_to :sent_to
-  belongs_to :sent_by
+  belongs_to :sent_to, class_name: :user, foreign_key: :sent_to_id
+  belongs_to :sent_by, class_name: :user, foreign_key: :sent_by_id
+
+  scope :friends, -> { where('status =?', true) }
+  scope :non_friends, -> { where('status =?', false) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-        
+
   private
+
   # validates the size of an uploaded picture
   def picture_size
     errors.add(:image, 'should be less than 1MB') if image.size > 1.megabytes
@@ -14,4 +15,22 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :likes, dependent: :destroy
+  has_many :friend_sent, class_name: :friendship,
+                         foreign_key: :sent_by_id,
+                         inverse_of: :sent_by,
+                         dependent: :destroy
+
+  has_many :friend_request, class_name: :friendship,
+                            foreign_key: :sent_to_id,
+                            inverse_of: :sent_to,
+                            dependent: :destroy
+
+  has_many :freinds, -> { merge(Friendship.freinds) },
+           through: :friend_sent, source: :sent_to
+
+  has_many :pending_requests, -> { merge(Friendship.non_friends) },
+           through: :friend_sent, source: :sent_to
+
+  has_many :received_requests, -> { merge(Friendship.non_friends) },
+           through: :friend_request, source: :sent_by
 end
