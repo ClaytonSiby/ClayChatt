f0f44b164a6352fa9f5d9e5337a76fb651bedd69
Clayton Siby <claytonsiby@gmail.com>
2020-10-20 10:17:37 +0200

    Generate Post model & validate file uploads

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
+  mount_uploader :image, ImageUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+        
+  private
+  # validates the size of an uploaded picture
+  def picture_size
+    errors.add(:image, 'should be less than 1MB') if image.size > 1.megabytes
+  end
 end
